809336c7a9fa1fc0859d18da4842d342124f28aa
yousof <yosofasady2@gmail.com>
2023-12-10 12:33:45 +0330

    fix structure

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'coffeeshop_custom',
+    'core',
     'foodmenu',
     'order',
-    'staff',
     'tables',
     'users',
     
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-
 # Create your models here.
+
+class UserModelManager(BaseUserManager):
+    def create_user(self,phone,password = None,**kwargs):
+        if phone :
+            user = self.model(phone = phone, **kwargs)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+        else:
+            raise Exception('requierd')
+    
+    def create_staff():pass
