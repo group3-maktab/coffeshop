158d145a74e5489e821de073ad15837faeea71c8
yousof <yosofasady2@gmail.com>
2023-12-18 12:48:52 +0330

    menu bar + logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,27 @@ module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
     extend: {
-      flexGrow: { 10:'10', 4:'4', 5:'5',
-        2: '2'},
-      fontFamily:{
-        enfont:['Nunito'],farsi:['Vazirmatn']
+      flexGrow: {
+        10: '10',
+        4: '4',
+        5: '5',
+        2: '2'
+      },
+      fontFamily: {
+        enfont: ['Nunito'],
+        farsi: ['Vazirmatn']
+      },
+      colors: {
+        cafe: {
+          black: '#0b0c10',
+          blue: '#45a29e',
+          cyan: '#66fcf1',
+          lime: '#49c5b6',
+          gray: '#1f2833',
+          white: '#c5c6c7',
+        },
       },
     },
   },
   plugins: [],
-}
-
+};
