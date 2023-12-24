a3943090075193fb99df2cb6f57402417a582a36
yousof <yosofasady2@gmail.com>
2023-12-24 21:03:34 +0330

    make signal for tables making TakeAway table by default

--- tables/apps.py
+++ tables/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TablesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tables'
+
+    def ready(self):
+        import tables.singals
