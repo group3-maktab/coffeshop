06c57db68b1b35f08b75964a6172a60feb77729b
yousof <yosofasady2@gmail.com>
2023-12-29 02:15:29 +0330

    add user to the auditlog

--- core/signals.py
+++ core/signals.py
@@ -11,6 +11,7 @@ from django.db.models.fields.related import ForeignKey
 from django.db.models.fields.files import FileField
 from django.db.models.fields import DateTimeField
 import uuid
+import inspect
 
 """ 
    ________________________________________________________________________________
@@ -58,7 +59,7 @@ def serialize_model_instance(instance):
 @receiver(post_save, sender=Reservation)
 @receiver(post_save, sender=Order)
 def log_create_update(sender, instance, created, **kwargs):
-    model_name = sender.__name__
+    model_name = sender.__name__  #just for fun :-|
 
 
     if created:
@@ -70,15 +71,20 @@ def log_create_update(sender, instance, created, **kwargs):
 
     table_name = sender._meta.db_table
     row_id = instance.id
+    """                                                                                               """"""
+        |                                                                                                   |        
+        |        https://stackoverflow.com/questions/4721771/get-current-user-log-in-signal-in-django       |
+        |                                                                                                   |
+        """                                                                                               """"""
+    # refrence is up here
+    for frame_record in inspect.stack():
+        if frame_record[3] == 'get_response':
+            request = frame_record[0].f_locals['request']
+            """https://docs.python.org/3/library/inspect.html"""
+            """https://www.geeksforgeeks.org/inspect-module-in-python/"""
+            break
 
-    if hasattr(instance, 'user'):
-        user = instance.user
-    else:
-        # If user is not available in the model, try to get it from the request
-        request = kwargs.get('request')
-        if request and request.user.is_authenticated:
-            user = request.user
-        user = None
+    user = request.user
 
     AuditLog.objects.create(
         user=user,
@@ -97,20 +103,26 @@ def log_create_update(sender, instance, created, **kwargs):
 @receiver(pre_delete, sender=Reservation)
 @receiver(pre_delete, sender=Order)
 def log_delete(sender, instance, **kwargs):
-    model_name = sender.__name__
+    model_name = sender.__name__ #just for fun :-|
     if isinstance(instance, Session) or isinstance(instance, AuditLog):
         return
 
     table_name = sender._meta.db_table
     row_id = instance.id
+    """                                                                                               """"""
+    |                                                                                                   |        
+    |        https://stackoverflow.com/questions/4721771/get-current-user-log-in-signal-in-django       |
+    |                                                                                                   |
+    """                                                                                               """"""
+    # refrence is up here
+    for frame_record in inspect.stack():
+        if frame_record[3] == 'get_response':
+            request = frame_record[0].f_locals['request']
+            """https://docs.python.org/3/library/inspect.html"""
+            """https://www.geeksforgeeks.org/inspect-module-in-python/"""
+            break
 
-    if hasattr(instance, 'user'):
-        user = instance.user
-    else:
-        # If user is not available in the model, try to get it from the request
-        request = kwargs.get('request')
-        if request and request.user.is_authenticated:
-            user = request.user
+    user = request.user
 
     old_value = serialize_model_instance(instance)
 
