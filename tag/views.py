51650404d5278b8b61733d72772503473c255213
yousof <yosofasady2@gmail.com>
2023-12-30 16:54:29 +0330

    working on tags list front

--- tag/views.py
+++ tag/views.py
@@ -1,10 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, CreateView, DeleteView
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Tag
 
 
