c83c6a6388b7ccd1e509d065037cacda520d0492
yousof <yosofasady2@gmail.com>
2023-12-31 13:18:18 +0330

    Reservation modified done: # CreateFood

--- foodmenu/forms.py
+++ foodmenu/forms.py
@@ -11,19 +11,26 @@ class CategoryCreateForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name', 'parent']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'parent': forms.Select(attrs={'class': 'form-select'}),
+        }
 
 
 class FoodCreateForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'flex-row p-2 form-check-input h-100 w-100'}),
         required=False,
     )
 
-    off = forms.IntegerField(max_value=100,min_value=0)
-    price = forms.DecimalField(max_value=10000, min_value=0)
+    off = forms.IntegerField(max_value=100, min_value=0, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    price = forms.DecimalField(max_value=10000, min_value=0,widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Food
         fields = ['name', 'price', 'off', 'category', 'tags']
-
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'btn btn-info'}),
+        }
